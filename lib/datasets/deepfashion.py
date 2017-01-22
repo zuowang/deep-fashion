b69d17447badf98b0ec63742d5e679fdc29f1054
Zuo Wang <wangzuo.nj@qq.com>
2017-01-22 16:25:35 +0800

    add DeepFashion dataset

---
+++ lib/datasets/deepfashion.py
@@ -0,0 +1,252 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Zuo Wang
+# --------------------------------------------------------
+
+import os
+import errno
+from datasets.imdb import imdb
+import xml.dom.minidom as minidom
+import numpy as np
+import scipy.sparse
+import scipy.io as sio
+import utils.cython_bbox
+import cPickle
+import subprocess
+import uuid
+from inria_eval import inria_eval
+
+class deepfashion(imdb):
+    def __init__(self, image_set, devkit_path):
+        imdb.__init__(self, image_set)
+        self._image_set = image_set
+        self._devkit_path = devkit_path
+        self._data_path = os.path.join(self._devkit_path, 'Consumer2Shop')
+        self._classes = ('__background__', # always index 0
+                         'clothing')
+        self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
+        self._image_ext = '.jpg'
+        self._image_index = self._load_image_set_index()
+        self._bbox_index = self._load_bbox_index()
+        self._salt = str(uuid.uuid4())
+        self._comp_id = 'comp4'
+
+        # Specific config options
+        self.config = {'cleanup'  : True,
+                       'use_salt' : True,
+                       'top_k'    : 2000,
+                       'use_diff' : False,
+                       'rpn_file' : None}
+
+        assert os.path.exists(self._devkit_path), \
+                'Devkit path does not exist: {}'.format(self._devkit_path)
+        assert os.path.exists(self._data_path), \
+                'Path does not exist: {}'.format(self._data_path)
+
+    def image_path_at(self, i):
+        """
+        Return the absolute path to image i in the image sequence.
+        """
+        return self.image_path_from_index(self._image_index[i])
+
+    def image_path_from_index(self, index):
+        """
+        Construct an image path from the image's "index" identifier.
+        """
+        for ext in self._image_ext:
+            image_path = os.path.join(self._data_path, 'Consumer2Shop',
+                                  index + ext)
+            if os.path.exists(image_path):
+                break
+        assert os.path.exists(image_path), \
+                'Path does not exist: {}'.format(image_path)
+	return image_path
+
+    def _load_image_set_index(self):
+        """
+        Load the indexes listed in this dataset's image set file.
+        """
+        # Example path to image set file:
+        # self._data_path + /Consumer2Shop/Anno/train.txt
+        image_set_file = os.path.join(self._data_path, 'Consumer2Shop', 'Anno', 
+                                      self._image_set + '.txt')
+        assert os.path.exists(image_set_file), \
+                'Path does not exist: {}'.format(image_set_file)
+        with open(image_set_file) as f:
+            image_index = [x.strip() for x in f.readlines()]
+        return image_index
+
+    def gt_roidb(self):
+        """
+        Return the database of ground-truth regions of interest.
+
+        This function loads/saves from/to a cache file to speed up future calls.
+        """
+        cache_file = os.path.join(self.cache_path, self.name + '_gt_roidb.pkl')
+        if os.path.exists(cache_file):
+            with open(cache_file, 'rb') as fid:
+                roidb = cPickle.load(fid)
+            print '{} gt roidb loaded from {}'.format(self.name, cache_file)
+            return roidb
+
+        gt_roidb = [self._load_inria_annotation(index)
+                    for index in self.image_index]
+        with open(cache_file, 'wb') as fid:
+            cPickle.dump(gt_roidb, fid, cPickle.HIGHEST_PROTOCOL)
+        print 'wrote gt roidb to {}'.format(cache_file)
+
+        return gt_roidb
+
+    def rpn_roidb(self):
+        gt_roidb = self.gt_roidb()
+        rpn_roidb = self._load_rpn_roidb(gt_roidb)
+        roidb = imdb.merge_roidbs(gt_roidb, rpn_roidb)
+        #roidb = self._load_rpn_roidb(None)
+        return roidb
+
+    def _load_rpn_roidb(self, gt_roidb):
+        filename = self.config['rpn_file']
+        print 'loading {}'.format(filename)
+        assert os.path.exists(filename), \
+               'rpn data not found at: {}'.format(filename)
+        with open(filename, 'rb') as f:
+            box_list = cPickle.load(f)
+        return self.create_roidb_from_box_list(box_list, gt_roidb)
+
+    def _load_bbox_index(self):
+        """
+        Load the indexes listed in this dataset's bbox file.
+        """
+        # self._data_path + /Consumer2Shop/Anno/bbox.txt
+        bbox_file = os.path.join(self._data_path, 'Consumer2Shop', 'Anno', 
+                                      'bbox.txt')
+        assert os.path.exists(bbox_file), \
+                'Path does not exist: {}'.format(bbox_file)
+        with open(bbox_file) as f:
+            bbox_index = [x.strip() for x in f.readlines()]
+        return bbox_index
+
+    def _load_deepfashion_annotation(self, index):
+        """
+        Load image and bounding boxes info from txt files of DeepFashion.
+        """
+
+        # Only one clothing in an image
+        num_objs = 1
+        boxes = np.zeros((num_objs, 4), dtype=np.uint16)
+        gt_classes = np.zeros((num_objs), dtype=np.int32)
+        overlaps = np.zeros((num_objs, self.num_classes), dtype=np.float32)
+
+        # "Seg" area here is just the box area
+        seg_areas = np.zeros((num_objs), dtype=np.float32)
+
+        # Load object bounding boxes into a data frame.
+        # Make pixel indexes 0-based
+	coor = re.findall('\d+', self._bbox_index[index])
+        x1 = float(coor[0])
+        y1 = float(coor[1])
+        x2 = float(coor[2])
+        y2 = float(coor[3])
+        cls = self._class_to_ind['clothing']
+        boxes[ix, :] = [x1, y1, x2, y2]
+        gt_classes[ix] = cls
+        overlaps[ix, cls] = 1.0
+        seg_areas[ix] = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+        overlaps = scipy.sparse.csr_matrix(overlaps)
+
+        return {'boxes' : boxes,
+                'gt_classes': gt_classes,
+                'gt_overlaps' : overlaps,
+                'flipped' : False,
+                'seg_areas' : seg_areas}
+
+    def _write_inria_results_file(self, all_boxes):
+        for cls_ind, cls in enumerate(self.classes):
+            if cls == '__background__':
+                continue
+            print 'Writing {} results file'.format(cls)
+            filename = self._get_inria_results_file_template().format(cls)
+            with open(filename, 'wt') as f:
+                for im_ind, index in enumerate(self.image_index):
+                    dets = all_boxes[cls_ind][im_ind]
+                    if dets == []:
+                        continue
+                    # the VOCdevkit expects 1-based indices
+                    for k in xrange(dets.shape[0]):
+                        f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                                format(index, dets[k, -1],
+                                       dets[k, 0] + 1, dets[k, 1] + 1,
+                                       dets[k, 2] + 1, dets[k, 3] + 1))
+
+    def evaluate_detections(self, all_boxes, output_dir):
+        self._write_inria_results_file(all_boxes)
+        self._do_python_eval(output_dir)
+        if self.config['cleanup']:
+            for cls in self._classes:
+                if cls == '__background__':
+                    continue
+                filename = self._get_inria_results_file_template().format(cls)
+                os.remove(filename)
+
+    def _get_comp_id(self):
+        comp_id = (self._comp_id + '_' + self._salt if self.config['use_salt']
+            else self._comp_id)
+        return comp_id
+
+    def _get_inria_results_file_template(self):
+        # INRIAdevkit/results/comp4-44503_det_test_{%s}.txt
+        filename = self._get_comp_id() + '_det_' + self._image_set + '_{:s}.txt'
+        try:
+            os.mkdir(self._devkit_path + '/results')
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                pass
+            else:
+                raise e
+        path = os.path.join(
+            self._devkit_path,
+            'results',
+            filename)
+        return path
+
+    def _do_python_eval(self, output_dir = 'output'):
+        annopath = os.path.join(
+            self._data_path,
+            'Annotations',
+            '{:s}.txt')
+        imagesetfile = os.path.join(
+            self._data_path,
+            'ImageSets',
+            self._image_set + '.txt')
+        cachedir = os.path.join(self._devkit_path, 'annotations_cache')
+        aps = []
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        for i, cls in enumerate(self._classes):
+            if cls == '__background__':
+                continue
+            filename = self._get_inria_results_file_template().format(cls)
+            rec, prec, ap = inria_eval(
+                filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5)
+            aps += [ap]
+            print('AP for {} = {:.4f}'.format(cls, ap))
+            with open(os.path.join(output_dir, cls + '_pr.pkl'), 'w') as f:
+                cPickle.dump({'rec': rec, 'prec': prec, 'ap': ap}, f)
+        print('Mean AP = {:.4f}'.format(np.mean(aps)))
+        print('~~~~~~~~')
+        print('Results:')
+        for ap in aps:
+            print('{:.3f}'.format(ap))
+        print('{:.3f}'.format(np.mean(aps)))
+        print('~~~~~~~~')
+        print('')
+        print('--------------------------------------------------------------')
+        print('Results computed with the **unofficial** Python eval code.')
+        print('Results should be very close to the official MATLAB eval code.')
+        print('Recompute with `./tools/reval.py --matlab ...` for your paper.')
+        print('-- Thanks, The Management')
+        print('--------------------------------------------------------------')
+
--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -11,6 +11,7 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.deepfashion import deepfashion
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -30,6 +31,11 @@ for year in ['2015']:
     for split in ['test', 'test-dev']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+deepfashion_path = '/home/deepinsight/py-faster-rcnn/data/DeepFashion'
+for split in ['train', 'test']:
+    name = '{}_{}'.format('deepfashion', split)
+    __sets[name] = (lambda split=split: deepfashion(split, deepfashion_path))
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
